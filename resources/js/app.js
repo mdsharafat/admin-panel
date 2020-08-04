e421afe5f27cbc2518354ab846e7078687e158e2
mdsharafat <sharafat.sohan047@gmail.com>
2020-08-04 21:44:00 +0600

    vuetify and router js file added and configured

--- resources/js/app.js
+++ resources/js/app.js
@@ -4,9 +4,11 @@
  * building robust, powerful web applications using Vue and Laravel.
  */
 
-require('./bootstrap');
+require("./bootstrap");
 
-window.Vue = require('vue');
+window.Vue = require("vue");
+import vuetify from "./vuetify";
+import router from "./router";
 
 /**
  * The following block of code may be used to automatically register your
@@ -19,7 +21,12 @@ window.Vue = require('vue');
 // const files = require.context('./', true, /\.vue$/i)
 // files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default))
 
-Vue.component('example-component', require('./components/ExampleComponent.vue').default);
+// Vue.component(
+//     "example-component",
+//     require("./components/ExampleComponent.vue").default
+// );
+
+import Example from "./components/ExampleComponent";
 
 /**
  * Next, we will create a fresh Vue application instance and attach it to
@@ -27,6 +34,11 @@ Vue.component('example-component', require('./components/ExampleComponent.vue').
  * or customize the JavaScript scaffolding to fit your unique needs.
  */
 
-const app = new Vue({
-    el: '#app',
+app = new Vue({
+    el: "#app",
+    router,
+    vuetify,
+    components: {
+        "example-component": Example
+    }
 });
